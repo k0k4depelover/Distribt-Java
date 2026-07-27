746af3633320d7ff7b5c6563101b4cea018a54ab
kok4depelover <oskarcermeno1@gmail.com>
2026-07-27 17:35:24 -0600

    Agregando cambios restantes

--- api-gateway-teenants/src/main/java/com/distribuited/systems/api_gateway_teenants/Entities/ApiKeyEntity.java
+++ api-gateway-teenants/src/main/java/com/distribuited/systems/api_gateway_teenants/Entities/ApiKeyEntity.java
@@ -4,11 +4,16 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "api_keys")
 public class ApiKeyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer Id;
 
